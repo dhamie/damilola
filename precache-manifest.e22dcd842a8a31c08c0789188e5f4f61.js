15548ffc257e84892a0f30362f178e67310f37c2
Soyemi Damilola <dhamie.soyemi@gmail.com>
2019-04-19 20:15:51 +0100

    Updates

---
+++ precache-manifest.e22dcd842a8a31c08c0789188e5f4f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7d675755a4ef7185327",
+    "url": "/damilola/static/js/runtime~main.36032dfa.js"
+  },
+  {
+    "revision": "5fb6a84070c76bf9d64a",
+    "url": "/damilola/static/js/main.d135e32b.chunk.js"
+  },
+  {
+    "revision": "4a6df10ebea09bf62e89",
+    "url": "/damilola/static/js/2.47e83f28.chunk.js"
+  },
+  {
+    "revision": "5fb6a84070c76bf9d64a",
+    "url": "/damilola/static/css/main.b4b1843c.chunk.css"
+  },
+  {
+    "revision": "4a6df10ebea09bf62e89",
+    "url": "/damilola/static/css/2.00013f1d.chunk.css"
+  },
+  {
+    "revision": "e85e93389cd797c24cc61c57b3e87f45",
+    "url": "/damilola/index.html"
+  }
+];
