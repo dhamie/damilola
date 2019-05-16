41246e759dcfb6b113c09078f9d6b818efb2d768
Soyemi Damilola <dhamie.soyemi@gmail.com>
2019-05-16 18:46:22 +0100

    counter test

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,6 @@
 import React, {Component } from 'react';
 import { Link } from 'react-router-dom';
-import CountdownTimer from "react-component-countdown-timer";
-import './pages/counter.css';
+import Countdown from "react-countdown-now";
 
 
 
@@ -10,19 +9,9 @@ import './pages/counter.css';
 
 class Footer extends Component {
     render() {
-
-          var settings = {
-          count: 5432,
-          border: true,
-          noPoints: false, border: false, direcion: "right",
-          size: 20,
-          showTitle: false,
-          backgroundColor: false,
-            color: "#fff",
-        };
         return (
           <div id="footer">
-            <CountdownTimer {...settings} />
+            <Countdown date={Date.now() + 10000} />,
           </div>
         );
     }
